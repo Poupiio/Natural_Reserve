d5602be56028c79533479221acbfb05f6a996d8a
Poupio <elise.maaiza33@gmail.com>
2023-01-16 12:25:27 +0100

    insert resa

--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -9,64 +9,61 @@ import style from '../modules/Reservation.module.css';
 const ReservationForm = () => {
     const navigate = useNavigate();
 
-    const [selectedValue, setSelectedValue] = useState("");
-    const [numPlaces, setNumPlaces] = useState("");
+    const [safariSelected, setSafariSelected] = useState();
+    const [numSelected, setNumSelected] = useState();
 
     const handleChange = (event) => {
-        setSelectedValue(event.target.value);
-    }
-
-    const selectedNumber = (ev) => {
-        setNumPlaces(ev.target.value);
+        setSafariSelected(event.target.value);
+        setNumSelected(event.target.value);
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const nb_places = numPlaces;
-        const user_name = e.target.user_name.value;
-        const safari_title = selectedValue;
-        const user_email = e.target.user_email.value;
-
-        fetch('http://localhost:80/api/reservations', {
+        const safari = safariSelected;
+        const places = numSelected;
+        const email = e.target.email.value;
+        const name = e.target.name.value;
+        
+        fetch("http://localhost:80/api/reservations", {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                nb_places,
-                user_name,
-                safari_title,
-                user_email
+                safari,
+                places,
+                name,
+                email
             })
         });
         alert("Votre demande de réservation a bien été enregistrée. Elle sera traitée dans les meilleurs délais.");
-        navigate("/safaris");
+        // navigate("/safaris");
     }
 
     return (
         <div className={style.bgi}>
             <Header />
-            <h2>Réservez votre safari</h2>
+            <h2>Réserver un safari</h2>
             <div className={style.formWrapper}>
                 <form onSubmit={handleSubmit}>
                     <div className="formContent">
                         <p>Pour quel safari souhaitez-vous effectuer une réservation ?</p>
-                        <input type="radio" name="safari" onChange={handleChange} />
+                        <input type="radio" name="safari" value="Standard" onChange={handleChange} />
                         <label className={style.label}>Standard</label>
 
-                        <input type="radio" name="safari" onChange={handleChange} />
+                        <input type="radio" name="safari" value="Deluxe" onChange={handleChange} />
                         <label className={style.label}>Deluxe</label>
 
-                        <input type="radio" name="safari" onChange={handleChange} />
+                        <input type="radio" name="safari" value="Premium" onChange={handleChange} />
                         <label className={style.label}>Premium</label>
 
-                        <input type="radio" name="safari" onChange={handleChange} />
+                        <input type="radio" name="safari" value="Platinum" onChange={handleChange} />
                         <label className={style.label}>Platinum</label>
                     </div>
                     <div className="selectNumber">
                         <p>Nombre de places souhaitées :</p>
-                        <input type="number" name="places" min="1" max="6" required onChange={selectedNumber} />
+                        <input type="number" name="places" min="1" max="6" onChange={handleChange} required />
                     </div>
                     <div className="formContent">
                         <label>Vos nom et prénom
